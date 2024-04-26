ad4c15b871be8a06e1853ccd167d784148221057
juxt_north <deykiron365@gmail.com>
2024-04-26 22:00:40 +0530

    Added pricing section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -125,4 +125,80 @@
 			</div>
 		</div>
 	</section>
+	<section class="space-y-8 p-4 text-center lg:space-y-16">
+		<h1 class="mx-auto text-3xl font-semibold md:max-w-[72%] md:text-5xl">
+			The most <span class="text-blue">value</span> for money
+			<span class="text-rose">educational</span> content you'll find on the planet.
+		</h1>
+		<div class="flex flex-col gap-4 px-8 md:grid md:grid-cols-2 max-w-[60rem] mx-auto">
+			<article class="w-full space-y-8 rounded-2xl bg-surface p-8 text-left flex flex-col justify-between">
+				
+					<div class="space-y-2">
+						<span class="font-semi-bold text-sm text-muted">Monthly</span>
+						<h1 class="text-3xl font-bold">
+							&#8377;899/<sub class="text-base text-muted">month</sub>
+						</h1>
+					</div>
+					<ul class="space-y-2">
+						<li class="flex h-fit gap-2">
+							<div class="mt-1 size-2 shrink-0 rounded-full bg-rose" />
+							<span class="leading-tight">Access to all courses.</span>
+						</li>
+						<li class="flex h-fit gap-2">
+							<div class="mt-1 size-2 shrink-0 rounded-full bg-rose" />
+							<span class="leading-tight">Access to member only blogs.</span>
+						</li>
+						<li class="flex h-fit gap-2">
+							<div class="mt-1 size-2 shrink-0 rounded-full bg-rose" />
+							<span class="leading-tight">Exclusive videos.</span>
+						</li>
+						<li class="flex h-fit gap-2">
+							<div class="mt-1 size-2 shrink-0 rounded-full bg-rose" />
+							<span class="leading-tight">Zero Ads</span>
+						</li>
+						<li class="flex h-fit gap-2">
+							<div class="mt-1 size-2 shrink-0 rounded-full bg-rose" />
+							<span class="leading-tight">Free exclusive merch sent to your doorstep.</span>
+						</li>
+						<li class="flex h-fit gap-2">
+							<div class="mt-1 size-2 shrink-0 rounded-full bg-rose" />
+							<span class="leading-tight">Live sessions.</span>
+						</li>
+						<li class="flex h-fit gap-2">
+							<div class="mt-1 size-2 shrink-0 rounded-full bg-rose" />
+							<span class="leading-tight">Cancel anytime</span>
+						</li>
+					</ul>
+				
+				<button class="w-full rounded-full bg-blue py-4 text-sm md:max-w-36 hover:brightness-110">Buy plan</button>
+			</article>
+			<div class="flex flex-col gap-4">
+				<article class="space-y-4 rounded-2xl bg-surface p-8 text-left">
+					<div class="space-y-2">
+						<span class="font-semi-bold text-sm text-muted">Yearly</span>
+						<h1 class="text-3xl font-bold">
+							&#8377;649/<sub class="text-base text-muted">month</sub>
+						</h1>
+					</div>
+					<p class="text-sm leading-tight">
+						Contains everything included in the monthly plan with discounts
+					</p>
+					<button class="w-full rounded-full bg-rose py-4 text-sm md:max-w-36 hover:brightness-110">Buy plan</button>
+				</article>
+				<article class="relative space-y-4 rounded-2xl bg-surface p-8 text-left [background-image:radial-gradient(circle_at_100%_100%,hsl(var(--blue)),hsl(var(--fuchsia)))]">
+					<div class="space-y-2 z-[1]">
+						<span class="font-semi-bold text-sm">Life membership</span>
+						<h1 class="text-3xl font-bold">
+							&#8377;4999
+						</h1>
+					</div>
+					<p class="text-sm leading-tight z-[1]">
+						Get a huge discount and unlimited access.
+					</p>
+					<button class="w-full rounded-full bg-white/20 hover:bg-white/30 py-4 text-sm z-[1] md:max-w-36">Buy plan</button>
+					<NoisyOverlay class="opacity-60" />
+				</article>
+			</div>
+		</div>
+	</section>
 </main>
