7fcfa55741ad2512865ac9c85c0c7053c98a815d
juxt_north <deykiron365@gmail.com>
2024-04-28 01:50:03 +0530

    Add meta tags to head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,25 +19,6 @@
 		href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
 		rel="stylesheet"
 	/>
-	<meta name="image" content="https://ik.imagekit.io/north01x/bscaiavuo/tr:w-720/banner.png" />
-	<meta property="og:title" content="Nine-Academy - Streamlined Education platform." />
-	<meta
-		property="og:image"
-		content="https://ik.imagekit.io/north01x/bscaiavuo/tr:w-720/banner.png"
-	/>
-	<meta
-		property="og:description"
-		content="Level up your coding skills with expertly crafted courses."
-	/>
-	<meta name="twitter:title" content="Nine-Academy - Streamlined Education platform" />
-	<meta
-		name="twitter:image"
-		content="https://ik.imagekit.io/north01x/bscaiavuo/tr:w-720/banner.png"
-	/>
-	<meta
-		name="twitter:description"
-		content="Level up your coding skills with expertly crafted courses."
-	/>
 </svelte:head>
 
 <slot />
