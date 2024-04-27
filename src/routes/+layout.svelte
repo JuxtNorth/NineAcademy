6a0317afddd422ad171107b47e5cea3d676ef44a
juxt_north <deykiron365@gmail.com>
2024-04-28 01:42:29 +0530

    Update banner image link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,7 +21,7 @@
 	/>
 	<meta name="image" content="https://ik.imagekit.io/north01x/bscaiavuo/tr:w-720/banner.png" />
 		<meta property="og:title" content="Nine-Academy - Streamlined Education platform." />
-		<meta property="og:image" content="/banner.png" />
+		<meta property="og:image" content="https://ik.imagekit.io/north01x/bscaiavuo/tr:w-720/banner.png" />
 		<meta property="og:description" content="Level up your coding skills with expertly crafted courses." />
 </svelte:head>
 
