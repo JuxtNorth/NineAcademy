51a09f82111d5fd47b265716e30e7347dfc63343
juxt_north <deykiron365@gmail.com>
2024-04-28 21:14:50 +0530

    Fix icon URLs on icon grid

--- src/components/IconGrid.svelte
+++ src/components/IconGrid.svelte
@@ -1,22 +1,22 @@
 <div {...$$restProps}>
 	<div class="grid grid-cols-3 grid-rows-3 gap-2">
 		<div class="icon-container col-start-3 row-start-1">
-			<img class="scale-[1.2]" src="logos\svelte.svg" alt="Svelte Icon" />
+			<img class="scale-[1.2]" src="/logos/svelte.svg" alt="Svelte Icon" />
 		</div>
 		<div
 			class="icon-container col-start-2 row-start-2
     "
 		>
-			<img src="logos\astro.svg" alt="Astro Icon" class="scale-[0.8]" />
+			<img src="/logos/astro.svg" alt="Astro Icon" class="scale-[0.8]" />
 		</div>
 		<div class="icon-container col-start-3 row-start-2">
-			<img class="rounded-md" src="logos\typescript.svg" alt="Typescript Icon" />
+			<img class="rounded-md" src="/logos/typescript.svg" alt="Typescript Icon" />
 		</div>
 		<div class="icon-container col-start-2 row-start-3">
-			<img class="scale-[1.24]" src="logos\react.svg" alt="React Icon" />
+			<img class="scale-[1.24]" src="/logos/react.svg" alt="React Icon" />
 		</div>
 		<div class="icon-container col-start-1 row-start-3">
-			<img class="size-full scale-[0.9] invert" src="logos\next-js.svg" alt="Next JS Icon" />
+			<img class="size-full scale-[0.9] invert" src="/logos/next-js.svg" alt="Next JS Icon" />
 		</div>
 	</div>
 </div>
