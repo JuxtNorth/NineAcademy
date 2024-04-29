174b21cd7cf886e9660b557618a8aab59ba1185f
juxt_north <deykiron365@gmail.com>
2024-04-30 00:15:26 +0530

    Remove Mark as Complete option as it is incomplete

--- src/components/Pagination.svelte
+++ src/components/Pagination.svelte
@@ -1,19 +1,10 @@
 <script lang="ts">
 	import { Button } from './ui';
-	import { Check, AngleLeft, AngleRight } from '$icons';
+	import { AngleLeft, AngleRight } from '$icons';
 	import { cn } from '$lib/utils';
-	import { alert } from '$lib/stores';
 	import { createEventDispatcher } from 'svelte';
 
 	export let editLink: string | undefined;
-
-	function showCompleteMessage() {
-		alert.set({
-			title: 'info',
-			description: 'This feature is coming soon',
-			type: 'info'
-		});
-	}
 
 	const dispatch = createEventDispatcher();
 </script>
@@ -27,11 +18,6 @@
 			>
 		</li>
 	{/if}
-	<li>
-		<Button class="flex h-full items-center gap-2 text-sm" on:click={showCompleteMessage}
-			><Check /><span>Mark as Complete</span></Button
-		>
-	</li>
 	<li>
 		<Button size="icon" style="mono" class="rounded-full" on:click={() => dispatch('prev')}
 			><AngleLeft /></Button
