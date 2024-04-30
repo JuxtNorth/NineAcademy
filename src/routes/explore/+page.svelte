6340b65267e58f759b03738f296d6840eff10c0a
juxt_north <deykiron365@gmail.com>
2024-04-30 16:33:17 +0530

    Fix: Concurrency issues in explore page

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -14,28 +14,28 @@
 
 	async function getOwnedCourseIDs(uid: string) {
 		const userDoc = await getFirestoreDoc<{ purchases: string[] }>(`users/${uid}`);
-		if (userDoc) {
-			ownedCourses = userDoc.purchases;
-		}
+		return userDoc?.purchases || [];
 	}
 
 	async function getCourseNames() {
 		const db = getFirestore(getFirebaseApp());
 		const courseQuery = query(collection(db, 'resource-names'), where('type', '==', 'course'));
 		const courseDocs = await getDocs(courseQuery);
-		ownedCourses = [];
 		courseDocs.forEach((doc) => {
 			courses.push({
 				id: doc.id,
 				title: doc.data().name
 			});
 		});
+		courses = courses;
 	}
+
+	$: console.log(courses);
 
 	user.subscribe(async (userData) => {
 		if (userData === null) return;
 		getCourseNames();
-		getOwnedCourseIDs(userData.uid);
+		ownedCourses = await getOwnedCourseIDs(userData.uid);
 	});
 </script>
 
@@ -43,7 +43,7 @@
 <main class="mx-auto max-w-[100rem] space-y-8 p-8 text-center">
 	<h1 class="mx-auto text-3xl font-semibold md:max-w-[72%] md:text-5xl">Explore Courses</h1>
 	<section class="grid grid-flow-row gap-4 p-4 md:grid-cols-2 lg:grid-cols-3">
-		{#if ownedCourses.length > 0}
+		{#if courses.length > 0}
 			{#each courses as course, index}
 				<CourseTab {...course} owned={ownedCourses} variant={getBannerVariant(index)} />
 			{/each}
