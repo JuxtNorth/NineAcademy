7341c03fe119c092341ede71d9aff2980ac1840f
juxt_north <deykiron365@gmail.com>
2024-04-30 16:23:14 +0530

    Add helpful indicators to dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,8 +4,10 @@
 	import { getFirebaseApp, getFirestoreDoc } from '$lib/firebase';
 	import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
 	import { Nav } from '$components';
+	import { fade } from 'svelte/transition';
 
 	let ownedCourses: string[] = [];
+	let loadingState: 'progress' | 'complete' = 'progress';
 
 	let courses: Array<{ id: string; title: string }> = [];
 
@@ -15,7 +17,8 @@
 
 	async function getOwnedCourseIDs(uid: string): Promise<string[]> {
 		const userDoc = await getFirestoreDoc<{ purchases: string[] }>(`users/${uid}`);
-		return userDoc ? userDoc.purchases : [];
+		loadingState = 'complete';
+		return userDoc?.purchases ?? [];
 	}
 
 	async function getCourseNames(uid: string) {
@@ -39,25 +42,28 @@
 
 <Nav />
 <main class="mx-auto max-w-[100rem] space-y-8 p-8">
-	<h1
-		class="ml-4 text-3xl font-semibold md:max-w-[72%] md:text-5xl"
-	>
-		DashBoard
-	</h1>
+	<h1 class="ml-4 text-3xl font-semibold md:max-w-[72%] md:text-5xl">DashBoard</h1>
 	<div class="space-y-6 p-4 text-left">
 		<p class="text-2xl">Your Courses</p>
 		<section class="grid grid-flow-row gap-4 md:grid-cols-2 lg:grid-cols-3">
-			{#if courses.length > 0}
-				{#each courses as course, index}
-					<CourseTab {...course} owned={ownedCourses} variant={getBannerVariant(index)} />
-				{/each}
-			{:else}
+			{#if loadingState === 'progress'}
 				<CourseTab skeleton />
 				<CourseTab skeleton />
 				<CourseTab skeleton />
 				<CourseTab skeleton />
 				<CourseTab skeleton />
 				<CourseTab skeleton />
+			{:else if loadingState === 'complete'}
+				{#if courses.length === 0}
+					<div class="flex flex-col gap-4 items-center justify-center fixed top-0 left-0 h-screen w-screen bg-background" in:fade>
+						<p>You don't own any courses yet.</p>
+						<a href="/explore" class="px-5 py-3 bg-surface rounded-full block w-fit hover:brightness-125 transition-[filter] text-sm">Explore Courses</a>
+					</div>
+				{:else}
+					{#each courses as course, index}
+						<CourseTab {...course} owned={ownedCourses} variant={getBannerVariant(index)} />
+					{/each}
+				{/if}
 			{/if}
 		</section>
 	</div>
