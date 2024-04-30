18112e015415b85fca44aa790063149b185adc83
juxt_north <deykiron365@gmail.com>
2024-04-30 16:43:52 +0530

    Remove padding from create/blog container

--- src/routes/create/blog/+page.svelte
+++ src/routes/create/blog/+page.svelte
@@ -52,7 +52,7 @@
 	}
 </script>
 
-<main class="mx-auto mt-8 max-w-[80rem] space-y-8 lg:mb-12 p-4">
+<main class="mx-auto mt-8 max-w-[80rem] space-y-8 lg:mb-12 p-4 lg:p-0">
 	<h1 class="text-3xl">Create a blog</h1>
 	<section class="grid grid-flow-row lg:grid-cols-2 gap-8">
 		<form class="space-y-4">
